0a4e9e261d17042d41210a3867c7ad57d4accf25
zzz-lq <1312881142@qq.com>
2022-12-12 09:46:14 +0800

    context+reducer

--- src/contexts/cart.context.jsx
+++ src/contexts/cart.context.jsx
@@ -1,5 +1,5 @@
-import { useEffect } from "react"
-import { createContext,useState } from "react"
+import { createContext,useReducer } from "react"
+import { createReducer } from "../utils/reducer/reducer"
 
 export const CartContext = createContext({
   open:false,
@@ -40,28 +40,102 @@ const removeCartItem = (cartItems,productToRemove) => {
   })
 }
 
+const delectCartItem = (cartItems,productDelect) =>{
+  cartItems.filter((cartItem) => (cartItem.id !== productDelect.id))
+}
+
+export const CART_ACTION_TYPES = {
+  SET_OPEN:"SET_OPEN",
+  SET_CAERITEMS:'SET_CAERITEMS',
+  // SET_CART_COUNT:'SET_CART_COUNT',
+  // SET_TOTAL_PRICE:'SET_TOTAL_PRICE',
+}
+
+
+const cartReducer = (state,action) => {
+  const {type,payload} = action
+
+  switch (type){
+    case CART_ACTION_TYPES.SET_OPEN:
+      return{
+        ...state,
+        open:payload,
+      }
+    case CART_ACTION_TYPES.SET_CAERITEMS:
+      return{
+        ...state,
+        ...payload
+      }
+    // 单个更新太多琐碎代码，直接一次更新这三个的内容，因为items变化count、price也一定会变化，一起更新即可
+    // case CART_ACTION_TYPES.SET_CAERITEMS:
+    //   return{
+    //     ...state,
+    //     cartItems:payload
+    //   }
+    // case CART_ACTION_TYPES.SET_CART_COUNT:
+    //   return{
+    //     ...state,
+    //     cartCount:payload,
+    //   }
+    // case CART_ACTION_TYPES.SET_TOTAL_PRICE:
+    //   return{
+    //     ...state,
+    //     totalPrice:payload,
+    //   }
+    default:
+      throw new Error(`Unhandle type of ${type} in CartReducer`)
+  }
+}
+
+// reducer中只包含可读的值
+const Init_state = {
+  open:false,
+  cartItems:[],
+  cartCount:0,
+  totalPrice:0,
+}
+
 export const CartProvider = ({children}) => {
 
-  const [open,setOpen] = useState(false)
-  const [cartItems,setCartItems] = useState([])
-  const [cartCount,setCartCount] = useState(0);
-  const [totalPrice,setTotalPrice] = useState(0);
+  const [{open,cartItems,cartCount,totalPrice},dispath] = useReducer(cartReducer,Init_state)
 
-  useEffect(()=>{
-    const newCartCount = cartItems.reduce((total,cartItem) => total+cartItem.quantity,0)
-    setCartCount(newCartCount)
-    const newTotoalPrice = cartItems.reduce((total,cartItem) => (total+cartItem.quantity*cartItem.price),0)
-    setTotalPrice(newTotoalPrice)
-  },[cartItems])
+  const setOpen = (open) => {
+    dispath(CART_ACTION_TYPES.SET_OPEN,open)
+  }
+
+  const updateCartItemsReducer = (newCartItems) => {
+    const newCartCount = newCartItems.reduce((total,cartItem) => total+cartItem.quantity,0)
+    const newTotoalPrice = newCartItems.reduce((total,cartItem) => (total+cartItem.quantity*cartItem.price),0)
+
+    dispath(createReducer(CART_ACTION_TYPES.SET_CAERITEMS,
+      {cartItems:newCartItems,
+      cartCount:newCartCount,
+      totalPrice:newTotoalPrice,}
+    ))
+  }
+  // const setCartItems = (cartItems) => {
+  //   dispath({type:CART_ACTION_TYPES.SET_CAERITEMS,action:cartItems})
+  // }
+  // const setCartCount = (count) => {
+  //   dispath({type:CART_ACTION_TYPES.SET_CART_COUNT,action:count})
+  // }
+  // const setTotalPrice = (totalPrice) => {
+  //   dispath({type:CART_ACTION_TYPES.SET_TOTAL_PRICE,action:totalPrice})
+  // }
+  // const [open,setOpen] = useState(false)
+  // const [cartItems,setCartItems] = useState([])
+  // const [cartCount,setCartCount] = useState(0);
+  // const [totalPrice,setTotalPrice] = useState(0);
+
 
   const addItemToCart = (productToAdd) => { 
-    setCartItems(addCartItem(cartItems,productToAdd))
+    updateCartItemsReducer(addCartItem(cartItems,productToAdd))
   }
   const removeItemToCart = (productToRemove) => {
-    setCartItems(removeCartItem(cartItems,productToRemove))
+    updateCartItemsReducer(removeCartItem(cartItems,productToRemove))
   }
   const delectItemToCart = (productDelect) => {
-    setCartItems(cartItems.filter((cartItem) => (cartItem.id !== productDelect.id)))
+    updateCartItemsReducer(delectCartItem(cartItems,productDelect))
   }
   
   const value = {open,setOpen,cartItems,addItemToCart,cartCount,removeItemToCart,delectItemToCart,totalPrice}
---
+++ src/utils/reducer/reducer.js
@@ -0,0 +1,2 @@
+
+export const createReducer = (type,payload) => ({type,payload})
