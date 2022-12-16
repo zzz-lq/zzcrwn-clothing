1765c68715892af3f28a99165f4db8641c4f7506
zzz-lq <1312881142@qq.com>
2022-12-16 20:14:39 +0800

    adaptive

--- src/component/authentication/authentication.style.tsx
+++ src/component/authentication/authentication.style.tsx
@@ -5,4 +5,8 @@ export const AuthenticationContainer = styled.div`
   width: 900px;
   justify-content: space-between;
   margin: 30px auto;
+
+  @media screen and (max-width: 800px) {
+    flex-direction: column
+  }
 `
